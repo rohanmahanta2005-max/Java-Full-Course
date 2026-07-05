0c99ac0d8fe513f287334cfb634f20f5152feece
Rohan Mahanta <rohanmahanta2005@gmail.com>
2026-07-05 22:10:37 +0530

    new package added ,fibonacci series

---
+++ src/com/rohan/fibo.java
@@ -0,0 +1,24 @@
+package com.rohan;
+import java.util.Scanner;
+
+public class fibo {
+    public static void main(String[] args){
+        System.out.println("Fibonacci series");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int a=0;
+        int b = 1;
+        int sum;
+
+        for(int i=1; i<=n; i++){
+            System.out.println(a);
+            sum=a+b;
+            a = b;
+            b = sum;
+
+
+        }
+
+    }
+}
