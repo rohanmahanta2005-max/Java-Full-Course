fff82cc3441cc0accd1841c6ae393af5e1f14c6a
Rohan Mahanta <rohanmahanta2005@gmail.com>
2026-07-06 00:33:13 +0530

    occurance program

---
+++ src/com/rohan/occurance.java
@@ -0,0 +1,25 @@
+package com.rohan;
+
+import java.util.Scanner;
+
+public class occurance {
+    static void main(String[] args) {
+        System.out.println("occurance");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.print("Enter digit which you wanna see occurance: ");
+        int m = sc.nextInt();
+
+        int count = 0;
+        while (n != 0) {
+            int lastdigit = n % 10;
+            if (lastdigit == m) {
+                count++;
+            }
+            n = n / 10;
+        }
+
+        System.out.println(count);
+    }
+}
