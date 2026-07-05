14f2f68add35a0537107eb000f2bbdb00a33a18c
Rohan Mahanta <rohanmahanta2005@gmail.com>
2026-07-05 23:00:46 +0530

    Update Fibonacci program

--- src/com/rohan/fibo.java
+++ src/com/rohan/fibo.java
@@ -9,7 +9,7 @@ public class fibo {
         int n = sc.nextInt();
         int a=0;
         int b = 1;
-        int sum;
+        int sum=0;
 
         for(int i=1; i<=n; i++){
             System.out.println(a);
