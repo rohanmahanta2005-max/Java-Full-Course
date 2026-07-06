2d0b67e9cef4c949d24e1b7a843dd816e3c25563
Rohan Mahanta <rohanmahanta2005@gmail.com>
2026-07-06 21:17:32 +0530

    Added Nested Switch case program

---
+++ src/com/rohan/NestedSwitch.java
@@ -0,0 +1,39 @@
+package com.rohan;
+
+import java.util.Scanner;
+
+public class NestedSwitch {
+    public static void main(String[] args) {
+        System.out.println("Nested Switch");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Employee ID: ");
+        int empid = sc.nextInt();
+        System.out.print("Enter Department: ");
+        String department = sc.next();
+
+
+        switch(empid){
+            case 1:
+                System.out.println("Rohan Singh");
+                break;
+            case 2:
+                System.out.println("Rohan Kumar");
+                break;
+            case 3:   System.out.println("Employee number 3");
+
+                switch(department){
+                    case "It":
+                        System.out.println("IT department");
+                        break;
+                    case "Mechanical":
+                        System.out.println("Mechanical department ");
+                        break;
+
+                }
+                break;
+            default:
+                System.out.println("Please enter a valid Employee id ...");
+        }
+
+    }
+}
