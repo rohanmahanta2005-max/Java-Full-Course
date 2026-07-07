eb4b1924258f413ba427d934808a5e143d4562f7
Rohan Mahanta <rohanmahanta2005@gmail.com>
2026-07-07 21:29:59 +0530

    Added methods module

---
+++ src/Methods/src/Sum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args){
+
+        int ans = sum1();
+        System.out.print("The sum is " + ans);
+    }
+
+
+
+    static int sum1(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = input.nextInt();
+        return num1+num2;
+    }
+}
+
+
+
+
+
