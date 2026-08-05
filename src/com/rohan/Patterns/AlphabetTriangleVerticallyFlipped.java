968098fca7ef8ed41aa1e7a77b7236d80bc5a7ca
Rohan Mahanta <rohanmahanta2005@gmail.com>
2026-08-05 12:36:43 +0530

    patterns added

---
+++ src/com/rohan/Patterns/AlphabetTriangleVerticallyFlipped.java
@@ -0,0 +1,20 @@
+package com.rohan.Patterns;
+
+public class AlphabetTriangleVerticallyFlipped {
+    public static void main(String[] args){
+
+        System.out.print("Enter rows: ");
+        int n = input.num();
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print((char)(j+64) + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ src/com/rohan/Patterns/NumberTriangleVerticallyFlipped.java
@@ -0,0 +1,22 @@
+package com.rohan.Patterns;
+
+public class NumberTriangleVerticallyFlipped {
+    public static void main(String[] args){
+
+        System.out.print("Enter rows: ");
+        int n = input.num();
+
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print( j + " ");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
