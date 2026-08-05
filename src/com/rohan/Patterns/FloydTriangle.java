d4f3d09ac97deb44c105b367317aeb4db81d0731
Rohan Mahanta <rohanmahanta2005@gmail.com>
2026-08-05 10:46:15 +0530

    patterns

--- src/com/rohan/Patterns/FloydTriangle.java
+++ src/com/rohan/Patterns/FloydTriangle.java
@@ -4,7 +4,7 @@ public class FloydTriangle {
     public static void main(String[] args) {
         System.out.print("Enter Rows: ");
         int row = input.num();
-        int a = 1;
+        int a = 1;   //Floyd's Beauty
 
         for(int i=1; i<=row; i++){
             for(int j=1; j<=i; j++){
