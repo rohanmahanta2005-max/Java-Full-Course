841aa449960cddbeb3320d268ac45c84e6aada2c
Rohan Mahanta <rohanmahanta2005@gmail.com>
2026-08-12 12:19:22 +0530

    modified

--- src/Segragates0sand1s.java
+++ src/Segragates0sand1s.java
@@ -3,13 +3,9 @@ public class Segragates0sand1s {
         System.out.println("Segragate zeros to the left and ones to the right");
         int[] arr = {0,1,1,1,0,0,0,1,0,1,0};
         int numOfzeros = 0;
-        int numOfones = 0;
-        int i =0;
-        while(i<arr.length){
-          if(arr[i] == 0) numOfzeros++;
-          else numOfones++;
-          i++;
-        }
+      for(int ele: arr){
+          if(ele==0) numOfzeros++;
+      }
         for(int j=0; j<numOfzeros; j++) arr[j] = 0;
         for(int j=numOfzeros ;j<arr.length; j++) arr[j] = 1;
 
